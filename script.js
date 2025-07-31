806cca6e207d08f99155d8793406ecff9271fbfa
Andy Sarne <sarneandy6@gmail.com>
2025-07-31 14:29:25 +0800

    structuring quiz

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //below are the tutorial code
 const closeTutorial = document.getElementById("tutorial-close");
 const tutorialBackground = document.getElementById("tutorial-background");
+
 closeTutorial.addEventListener("click", async (e) => {
   e.preventDefault();
-
   tutorialBackground.classList.add("closeElement");
   await setTimeout(() => {
     tutorialBackground.style.display = "none";
@@ -12,25 +12,42 @@ closeTutorial.addEventListener("click", async (e) => {
 
 //below are the form code
 
-const category = document.getElementById('category');
-const difficulty = document.getElementById('difficulty')
-const amount = document.getElementById('amount');
+const category = document.getElementById("category");
+const difficulty = document.getElementById("difficulty");
+const amount = document.getElementById("amount");
 
-let quizAPI; 
+let quizAPI;
 
-const submitButton = document.getElementById('submit-form');
-submitButton.addEventListener('click', (e) => { //this might be submit || click
+const submitButton = document.getElementById("submit-form");
+
+submitButton.addEventListener("click", (e) => {
+  //this might be submit || click
   e.preventDefault();
   //the code to set the value for each user input
-  const userCategory = category.value;
-  const userDifficulty = difficulty.value;
-  const userAmount = amount.value;
-  console.log(userAmount, userDifficulty, userCategory)
-  quizAPI = `https://opentdb.com/api.php?amount=${userAmount}&category=${userCategory}&difficulty=${userDifficulty}&type=multiple`;
 
+  const userAmount = amount.value;
+  if (userAmount <= 10 && userAmount >= 1) {
+    const userCategory = category.value;
+    const userDifficulty = difficulty.value;
+    console.log(userAmount, userDifficulty, userCategory);
+    quizAPI = `https://opentdb.com/api.php?amount=${userAmount}&category=${userCategory}&difficulty=${userDifficulty}&type=multiple`;
+
+    //closing form
+    const form = document.getElementById("form-container");
+    form.classList.add("closeElement");
+    setTimeout(() => {
+      form.style.display = "none";
+    }, 1000);
+
+    
+
+  } else {
+    amount.style.borderColor = 'red';
+    return
+  }
 });
 
-console.log(quizAPI)
+console.log(quizAPI);
 
 //url format: https://opentdb.com/api.php?amount=10&category=24&difficulty=easy&type=multiple
 /*
@@ -46,12 +63,10 @@ type=multiple
 let data;
 const setQuestion = async () => {
   try {
-    const res = await fetch(quizAPI)
+    const res = await fetch(quizAPI);
     data = await res.json();
-    console.log(data.results[0].question);
+    console.log(data.results[0]);
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
-
-
+};
